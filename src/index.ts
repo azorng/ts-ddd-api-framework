9708e644db06dda9de68d1438ed405aec2c2ab06
Azor Navarro <azorvk@gmail.com>
2019-06-30 13:21:11 +0200

    Rename InitInfra

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'module-alias/register'
 import 'reflect-metadata';
-import initInfra from '~/infra'
+import initInfra from '~/infra/InitInfra'
 import Server from '~/ui/Server'
 
 (async () => {
