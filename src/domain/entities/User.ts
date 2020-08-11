d9b218c122c2d495be10970eb0811c35ce3957c1
Azor <azorvk@gmail.com>
2020-08-11 21:26:57 +0200

    Format

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,37 +1,37 @@
-import { Entity, Column, PrimaryGeneratedColumn, Generated } from "typeorm";
-import { Length, IsEmail } from "class-validator";
-import { EntityBase } from "~/domain/EntityBase";
-import { ExceptionCode } from "~/domain/exceptions/ExceptionMessages";
+import { Entity, Column, PrimaryGeneratedColumn, Generated } from 'typeorm'
+import { Length, IsEmail } from 'class-validator'
+import { EntityBase } from '~/domain/EntityBase'
+import { ExceptionCode } from '~/domain/exceptions/ExceptionMessages'
 
 export interface UserProps {
-  email: string;
-  password: string;
+    email: string
+    password: string
 }
 
-@Entity("user")
+@Entity('user')
 export class User extends EntityBase {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number
 
-  @Column()
-  @Generated("uuid")
-  uuid: string;
+    @Column()
+    @Generated('uuid')
+    uuid: string
 
-  @Column({ unique: true })
-  @IsEmail(undefined, {
-    message: ExceptionCode[ExceptionCode.NOT_VALID_EMAIL],
-  })
-  email: string;
+    @Column({ unique: true })
+    @IsEmail(undefined, {
+        message: ExceptionCode[ExceptionCode.NOT_VALID_EMAIL]
+    })
+    email: string
 
-  @Column({ select: false, length: 64 })
-  @Length(6, 64, {
-    message: ExceptionCode[ExceptionCode.PW_NOT_SECURE],
-  })
-  password: string;
+    @Column({ select: false, length: 64 })
+    @Length(6, 64, {
+        message: ExceptionCode[ExceptionCode.PW_NOT_SECURE]
+    })
+    password: string
 
-  constructor(user: UserProps) {
-    super();
-    this.email = user.email;
-    this.password = user.password;
-  }
+    constructor(user: UserProps) {
+        super()
+        this.email = user.email
+        this.password = user.password
+    }
 }
